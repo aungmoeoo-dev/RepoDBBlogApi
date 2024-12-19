432a87db50ffbbd53e6f17cfabb1d6631047b782
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-19 19:10:57 +0630

    feat: add BlogApiConsoleApp

---
+++ RepoDBBlogApi.ConsoleApp/Features/Blog/BlogService.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoDBBlogApi.ConsoleApp.Features.Blog;
+
+public class BlogService
+{
+	private readonly string endpoint = "https://localhost:7082/api/blog";
+	private readonly RestClient _restClient;
+
+	public BlogService()
+	{
+		_restClient = new RestClient();
+	}
+
+	public async Task<BlogListResponseModel> GetBlogs()
+	{
+		RestRequest request = new RestRequest(endpoint);
+		RestResponse response = await _restClient.ExecuteAsync(request);
+
+		string content = response.Content;
+		//Console.WriteLine(content);
+		return JsonConvert.DeserializeObject<BlogListResponseModel>(content);
+	}
+
+	public async Task<BlogResponseModel> GetBlog(string id)
+	{
+		RestRequest request = new($"{endpoint}/{id}");
+		RestResponse response = await _restClient.ExecuteAsync(request);
+		string content = response.Content;
+		//Console.WriteLine(content);
+		return JsonConvert.DeserializeObject<BlogResponseModel>(content);
+	}
+
+	public async Task<BlogResponseModel> CreateBlog(BlogModel requestModel)
+	{
+		RestRequest request = new(endpoint, Method.Post);
+		request.AddJsonBody(requestModel);
+		RestResponse response = await _restClient.ExecuteAsync(request);
+		string content = response.Content;
+		//Console.WriteLine(content);
+		return JsonConvert.DeserializeObject<BlogResponseModel>(content);
+	}
+
+	public async Task<BlogResponseModel> UpdateBlog(BlogModel requestModel)
+	{
+		RestRequest request = new($"{endpoint}/{requestModel.BlogId}", Method.Patch);
+		request.AddJsonBody(requestModel);
+
+		RestResponse response = await _restClient.ExecuteAsync(request);
+		string content = response.Content;
+		//Console.WriteLine(content);
+		return JsonConvert.DeserializeObject<BlogResponseModel>(content);
+	}
+
+	public async Task<BlogResponseModel> DeleteBlog(string id)
+	{
+		RestRequest request = new($"{endpoint}/{id}", Method.Delete);
+		RestResponse response = await _restClient.ExecuteAsync(request);
+
+		string content = response.Content;
+		//Console.WriteLine(content);
+		return JsonConvert.DeserializeObject<BlogResponseModel>(content);
+	}
+}
---
+++ RepoDBBlogApi.ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RepoDBBlogApi.ConsoleApp.Features.Blog;
+using System.Reflection.Metadata;
+
+BlogService blogService = new BlogService();
+
+var getBlogsResponse = await blogService.GetBlogs();
+foreach(var blog in getBlogsResponse.Data)
+{
+	Console.WriteLine(blog.BlogTitle);
+}
+
+Console.ReadLine();
+
+var getBlogResponse = await blogService.GetBlog("18d350f8-ca8f-48ac-a13f-49f0c47b2717");
+Console.WriteLine(getBlogResponse.Data.BlogTitle);
+
+Console.ReadLine();
+
+
+BlogModel newBlog = new()
+{
+	BlogTitle = "Test title",
+	BlogAuthor = "Test author",
+	BlogContent = "Test content"
+};
+var createBlogResponse = await blogService.CreateBlog(newBlog);
+Console.WriteLine(createBlogResponse.Data.BlogTitle);
+
+Console.ReadLine();
+
+BlogModel updateBlog = new()
+{
+	BlogId = "96de3635-a999-4260-936a-b4f7cb383e2a",
+	BlogTitle = "Test title",
+	BlogAuthor = "Test author",
+	BlogContent = "Test content"
+};
+var updateBlogResponse = await blogService.CreateBlog(updateBlog);
+Console.WriteLine(updateBlogResponse.Data.BlogTitle);
+
+Console.ReadLine();
+
+var deleteBlogResponse = await blogService.DeleteBlog("96de3635-a999-4260-936a-b4f7cb383e2a");
+Console.WriteLine(deleteBlogResponse.Message);
+
+Console.ReadLine();
