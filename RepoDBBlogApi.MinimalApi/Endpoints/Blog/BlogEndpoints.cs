9fc4fec2e72caa1360af615754de1990136d8d9c
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-19 18:43:41 +0630

    feat: add MinimalApi

---
+++ RepoDBBlogApi.MinimalApi/Endpoints/Blog/BlogEndpoints.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepoDBBlogApi.Shared;
+
+namespace RepoDBBlogApi.MinimalApi.Endpoints.Blog;
+
+public static class BlogEndpoints
+{
+	public static IEndpointRouteBuilder UseBlogEndpoint(this IEndpointRouteBuilder app)
+	{
+		app.MapGet("/api/Blog", () =>
+		{
+			BlogListResponseModel responseModel = new();
+			try
+			{
+				IBlogService blogService = new BlogService();
+				responseModel = blogService.GetBlogs();
+				return Results.Ok(responseModel);
+			}
+			catch (Exception ex)
+			{
+				responseModel.IsSuccess = false;
+				responseModel.Message = ex.Message;
+				return Results.Json(responseModel, statusCode: 500);
+			}
+		})
+		.WithName("GetBlogs")
+		.WithOpenApi();
+
+		app.MapGet("/api/Blog/{id}", (string id) =>
+		{
+			BlogResponseModel responseModel = new();
+			try
+			{
+				IBlogService blogService = new BlogService();
+				responseModel = blogService.GetBlog(id);
+				if (!responseModel.IsSuccess) return Results.BadRequest(responseModel);
+
+				return Results.Ok(responseModel);
+			}
+			catch (Exception ex)
+			{
+				responseModel.IsSuccess = false;
+				responseModel.Message = ex.Message;
+				return Results.Json(responseModel, statusCode: 500);
+			}
+		})
+		.WithName("GetBlog")
+		.WithOpenApi();
+
+		app.MapPost("/api/Blog", (TBL_Blog requestModel) =>
+		{
+			BlogResponseModel responseModel = new();
+			try
+			{
+				IBlogService blogService = new BlogService();
+				responseModel = blogService.CreateBlog(requestModel);
+				if (!responseModel.IsSuccess) return Results.BadRequest(responseModel);
+
+				return Results.Ok(responseModel);
+			}
+			catch (Exception ex)
+			{
+				responseModel.IsSuccess = false;
+				responseModel.Message = ex.Message;
+				return Results.Json(responseModel, statusCode: 500);
+			}
+		})
+		.WithName("CreateBlog")
+		.WithOpenApi();
+
+		app.MapPatch("/api/Blog/{id}", (string id, TBL_Blog requestModel) =>
+		{
+			BlogResponseModel responseModel = new();
+			try
+			{
+				IBlogService blogService = new BlogService();
+
+				requestModel.BlogId = id;
+				responseModel = blogService.UpdateBlog(requestModel);
+				if (!responseModel.IsSuccess) return Results.BadRequest(responseModel);
+
+				return Results.Ok(responseModel);
+			}
+			catch (Exception ex)
+			{
+				responseModel.IsSuccess = false;
+				responseModel.Message = ex.Message;
+				return Results.Json(responseModel, statusCode: 500);
+			}
+		})
+		.WithName("UpdateBlog")
+		.WithOpenApi();
+
+		app.MapDelete("/api/Blog/{id}", (string id) =>
+		{
+			BlogResponseModel responseModel = new();
+			try
+			{
+				IBlogService blogService = new BlogService();
+				responseModel = blogService.DeleteBlog(id);
+				if (!responseModel.IsSuccess) return Results.BadRequest(responseModel);
+
+				return Results.Ok(responseModel);
+			}
+			catch (Exception ex)
+			{
+				responseModel.IsSuccess = false;
+				responseModel.Message = ex.Message;
+				return Results.Json(responseModel, statusCode: 500);
+			}
+		})
+		.WithName("DeleteBlog")
+		.WithOpenApi();
+
+		return app;
+	}
+}
---
+++ RepoDBBlogApi.MinimalApi/Program.cs
@@ -0,0 +1,29 @@
+using RepoDBBlogApi.MinimalApi.Endpoints.Blog;
+using RepoDBBlogApi.Shared;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.UseBlogEndpoint();
+
+app.Run();
